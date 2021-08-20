fbc4180899ebee4435f1b5676035dd443c0e3d83
Naman Gairola <naman.gairola2001@gmail.com>
2021-08-20 19:43:37 +0530

    Create prob1.cpp

---
+++ week3/prob1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[100000],key,com,swa,i,j,n,m;
+    cin>>m;
+    while(m--)
+    {
+        com=0;
+        swa=0;
+        cin>>n;
+        for(i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        for(i=1;i<n;i++)
+        {
+            key=arr[i];
+            com++;
+            for(j=i ;j>0 && arr[j-1]>key;j--)
+            {
+                arr[j]=arr[j-1];
+                swa++;
+                com++;
+            }
+            arr[j]=key;
+        }
+        for(i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        cout<<"comparison="<<com<<"\n";
+        cout<<"swap="<<swa<<"\n";
+    }
+    return 0;
+}
